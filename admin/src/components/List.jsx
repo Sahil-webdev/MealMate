8716abe0e4df12a0707e8275ffd80ad065e1d8d9
sahil sharma <sahilshh777@gmail.com>
2025-04-08 00:45:18 +0530

    Update List.jsx

--- admin/src/components/List.jsx
+++ admin/src/components/List.jsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 
 const List = () => {
 
-  const url = "http://localhost:5000"
+  const url = "https://mealmate-backend-fddu.onrender.com"
   const [list, setList] = useState([]);
   const currency = "₹"
 
